5d903b7cad1deeb21a5b3c3ec2185f32367a1205
samuel-morgan-tyghe <sam.morgan@hedgehoglab.com>
2022-11-06 11:46:26 +0000

    init

---
+++ client/src/App.js
@@ -0,0 +1,30 @@
+// client/src/App.js
+
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => {
+        console.log("🚀 ~ file: App.js ~ line 19 ~ .then ~ res", res);
+        return res.json();
+      })
+      .then((data) => setData(data.message))
+      .catch((e) => console.log(e));
+  }, []);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{!data ? "Loading..." : data}</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
